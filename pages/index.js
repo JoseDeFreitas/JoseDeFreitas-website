b453f5b2eb1ed8b1cd13ec9ab6ef7da002b8e9cd
Jose De Freitas <joselocotote26@gmail.com>
2021-02-09 16:08:05 -0500

    Add content from docs

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
-import Head from 'next/head'
+import Link from "next/link";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Homepage - José De Freitas</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read{" "}
+          <Link href="/portfolio/development">
+            <a>this page!</a>
+          </Link>
         </h1>
 
         <p className="description">
@@ -54,7 +58,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -205,5 +209,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
