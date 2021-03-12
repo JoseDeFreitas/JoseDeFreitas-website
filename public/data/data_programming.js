13a60c43dfefc1f91de7b8c7ef23559345f57afa
Jose De Freitas <joselocotote26@gmail.com>
2021-03-12 09:41:45 -0500

    Create data files

---
+++ public/data/data_programming.js
@@ -0,0 +1,10 @@
+export const programmingProjects = [
+    {
+        id: 1,
+        name: 'destuck',
+        description: 'Desktop application to increase productivity allowing you to stop processes and see statistics of usage.',
+        creation: 'MM/YYYY',
+        link: 'https://github.com/JoseDeFreitas',
+        tags: ['Python', 'PyQt5', 'Desktop']
+    }
+]
---
+++ public/data/data_translation.js
@@ -0,0 +1,74 @@
+export const translationProjects = [
+    {
+        id: 1,
+        name: 'clean-code-typescript',
+        description: 'Clean Code concepts adapted for TypeScript (in Spanish).',
+        creation: '15/06/2020',
+        link: 'https://github.com/JoseDeFreitas/clean-code-typescript#readme',
+        pr: 'https://github.com/labs42io/clean-code-typescript/issues/15#issuecomment-644381510',
+        tags: ['TypeScript', 'guide']
+    },
+    {
+        id: 2,
+        name: 'github-readme-stats',
+        description: '⚡ Dynamically generated stats for your github readmes.',
+        creation: '20/07/2020',
+        link: 'https://github.com/anuraghazra/github-readme-stats/blob/master/docs/readme_es.md',
+        pr: 'https://github.com/anuraghazra/github-readme-stats/pull/116',
+        tags: ['readme', 'GitHub']
+    },
+    {
+        id: 3,
+        name: 'Melon',
+        description: 'UNES, o aplicativo que cria notificações para as ações do Portal Sagres.',
+        creation: '04/10/2020',
+        link: 'https://github.com/ForceTower/Melon/pull/79/files',
+        pr: 'https://github.com/ForceTower/Melon/pull/79',
+        tags: ['strings', 'application', 'Android']
+    },
+    {
+        id: 4,
+        name: 'awesome-translations',
+        description: '😎 Awesome lists about Internationalization & localization stuff. l10n, g11n, m17n, i18n. Translations! 🌎🌍.',
+        creation: '27/11/2020',
+        link: 'https://github.com/JoseDeFreitas/awesome-translations/tree/es_ES#readme',
+        pr: 'https://github.com/mbiesiad/awesome-translations/pull/10',
+        tags: ['translations', 'list']
+    },
+    {
+        id: 5,
+        name: 'ultimate-python',
+        description: 'Ultimate Python study guide for newcomers and professionals alike. 🐍 🐍 🐍.',
+        creation: '27/12/2020',
+        link: 'https://github.com/huangsam/ultimate-python/blob/master/README.es.md',
+        pr: 'https://github.com/huangsam/ultimate-python/pull/37',
+        tags: ['Python', 'guide']
+    },
+    {
+        id: 6,
+        name: 'learn-python',
+        description: '📚 Playground and cheatsheet for learning Python. Collection of Python scripts that are split by topics and contain code examples with explanations.',
+        creation: '18/01/2021',
+        link: 'https://github.com/JoseDeFreitas/learn-python/tree/spanish-translation#readme',
+        pr: 'https://github.com/trekhleb/learn-python/pull/31',
+        tags: ['Python', 'guide']
+    },
+    {
+        id: 7,
+        name: 'wtfpython',
+        description: 'What the f*ck Python? 😱 (in Spanish).',
+        creation: '24/02/2021',
+        link: 'https://github.com/JoseDeFreitas/wtfpython-es#readme',
+        pr: 'https://github.com/satwikkansal/wtfpython/pull/260',
+        tags: ['Python', 'guide']
+    },
+    {
+        id: 8,
+        name: 'learncpp.es',
+        description: 'Contenido de learncpp.com traducido al español.',
+        creation: 'DD/MM/YYYY',
+        link: 'https://github.com/JoseDeFreitas/learncpp.es#readme',
+        pr: null,
+        tags: ['C++', 'guide']
+    }
+]
