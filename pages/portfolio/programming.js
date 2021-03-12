d0cdf64515680eeb3baf669a74f5a3ea884b8833
Jose De Freitas <joselocotote26@gmail.com>
2021-03-12 10:50:02 -0500

    Write programming description

--- pages/portfolio/programming.js
+++ pages/portfolio/programming.js
@@ -1,7 +1,16 @@
 import Head from "next/head";
 import Layout from '../../components/Layout'
+import { programmingProjects } from "../../public/data/data_programming"
 
-export default function Programming() {
+export const getStaticProps = async () => {
+    return {
+        props: {
+            projectsList: programmingProjects,
+        }
+    }
+}
+
+export default function Programming({ projectsList }) {
     return (
         <>
             <Head>
@@ -9,7 +18,37 @@ export default function Programming() {
             </Head>
 
             <Layout>
-                <h1>Programming</h1>
+                <main id="main">
+                    <section id="header">
+                        <h1>Programming</h1>
+                        <p>The project I've been working on and that I've made.</p>
+                        <p>
+                            Each card consists of the project name, the project description, the project<br/>
+                            publication date (I update most of them when needed), the link to the project<br/>
+                            page (usually a GitHub repository) and some tags (including the programming<br/>
+                            language I've used).
+                        </p>
+                        <p>These project are of various topics and are open-source.</p>
+                    </section>
+                    <section id="container">
+                        {projectsList.map(project => (
+                            <div className="card">
+                                <div className="card-info">
+                                    <h3>{project.name}</h3>
+                                    <p>{project.description}</p>
+                                    <div className="card-extra">
+                                        <p class="date">{project.creation}</p>
+                                        <p class="card-tags">Tags: {project.tags.join(', ')}</p>
+                                    </div>
+                                </div>
+                                <hr/>
+                                <div className="card-links">
+                                    <a href={project.link}>Project</a>
+                                </div>
+                            </div>
+                        ))}
+                    </section>
+                </main>
             </Layout>
         </>
     )
