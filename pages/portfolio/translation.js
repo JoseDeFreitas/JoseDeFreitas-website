07066033c997f28e967f99ccdf3e4997cfa81782
Jose De Freitas <joselocotote26@gmail.com>
2021-03-12 09:57:45 -0500

    Render data content

--- pages/portfolio/translation.js
+++ pages/portfolio/translation.js
@@ -1,7 +1,16 @@
 import Head from "next/head";
 import Layout from '../../components/Layout'
+import { translationProjects } from '../../public/data/data_translation'
 
-export default function Translation() {
+export const getStaticProps = async () => {
+    return {
+        props: {
+            projectsList: translationProjects,
+        }
+    }
+}
+
+export default function Translation({ projectsList }) {
     return (
         <>
             <Head>
@@ -9,7 +18,28 @@ export default function Translation() {
             </Head>
 
             <Layout>
-                <h1>Translation</h1>
+                <main id="main">
+                    <section>
+                        <h1>Translation</h1>
+                        <p></p>
+                    </section>
+                    <section>
+                        {projectsList.map(project => (
+                            <div className="card">
+                                <div>
+                                    <h3>{project.name}</h3>
+                                    <p>{project.description}</p>
+                                    <p>{project.creation}</p>
+                                </div>
+                                <div>
+                                    <a href={project.link}>Translation</a>
+                                    <a href={project.pr}>Context</a>
+                                </div>
+                                <p>{project.tags}</p>
+                            </div>
+                        ))}
+                    </section>
+                </main>
             </Layout>
         </>
     )
