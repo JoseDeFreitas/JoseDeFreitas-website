3f81a16de60d78dad4d73b1d98c6c8ecca4e457a
Jose De Freitas <joselocotote26@gmail.com>
2021-03-15 11:43:01 -0500

    Remove line break

--- pages/portfolio/translation.js
+++ pages/portfolio/translation.js
@@ -35,7 +35,7 @@ export default function Translation({ projectsList }) {
                             from other projects. You can see the link to my Crowdin profile in the home page.
                         </p>
                         <p>
-                            <strong>I sell translations.</strong> You can go to <a href="https://ko-fi.com/s/4d65477558" target="_blank">my shop</a><br/>
+                            <strong>I sell translations.</strong> You can go to <a href="https://ko-fi.com/s/4d65477558" target="_blank">my shop</a>
                             to see the translation service I'm selling.
                         </p>
                     </section>
