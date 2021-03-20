7598204a970a70b58fa55bd46c1ccb64c8ffa66f
Jose De Freitas <joselocotote26@gmail.com>
2021-03-20 18:29:41 -0500

    Add two entries

--- public/data/data_programming.js
+++ public/data/data_programming.js
@@ -1,10 +1,18 @@
 export const programmingProjects = [
     {
         id: 1,
-        name: 'destuck',
-        description: 'Desktop application to increase productivity allowing you to stop processes and see statistics of usage.',
-        creation: 'DD/MM/YYYY',
-        link: 'https://github.com/JoseDeFreitas',
-        tags: ['Python', 'PyQt5', 'Desktop']
+        name: 'JoseDeFreitas website',
+        description: 'Portfolio website of José De Freitas (this one you are seeing).',
+        creation: '09/02/2021',
+        link: 'https://github.com/JoseDeFreitas/JoseDeFreitas-website',
+        tags: ['portfolio', 'website', 'frontend']
+    },
+    {
+        id: 2,
+        name: 'awesome-youtubers website voting system',
+        description: '▶️ An awesome list containing awesome YouTubers that teach about technology (website).',
+        creation: '07/03/2021',
+        link: 'https://github.com/JoseDeFreitas/awesome-youtubers/tree/main/src/voter',
+        tags: ['Flask', 'Python', 'database', 'api', 'backend']
     }
 ]
