7a8a508b969316ab1f153e1a4197ee03239a3c89
Jose De Freitas <joselocotote26@gmail.com>
2021-03-21 14:58:40 -0500

    Add original field

--- public/data/data_translation.js
+++ public/data/data_translation.js
@@ -4,7 +4,8 @@ export const translationProjects = [
         name: 'clean-code-typescript',
         description: 'Clean Code concepts adapted for TypeScript (in Spanish).',
         creation: '15/06/2020',
-        link: 'https://github.com/JoseDeFreitas/clean-code-typescript#readme',
+        original: 'https://github.com/labs42io/clean-code-typescript#readme',
+        translation: 'https://github.com/JoseDeFreitas/clean-code-typescript#readme',
         pr: 'https://github.com/labs42io/clean-code-typescript/issues/15#issuecomment-644381510',
         tags: ['TypeScript', 'guide']
     },
@@ -13,7 +14,8 @@ export const translationProjects = [
         name: 'github-readme-stats',
         description: '⚡ Dynamically generated stats for your github readmes.',
         creation: '20/07/2020',
-        link: 'https://github.com/anuraghazra/github-readme-stats/blob/master/docs/readme_es.md',
+        original: 'https://github.com/anuraghazra/github-readme-stats#readme',
+        translation: 'https://github.com/anuraghazra/github-readme-stats/blob/master/docs/readme_es.md',
         pr: 'https://github.com/anuraghazra/github-readme-stats/pull/116',
         tags: ['readme', 'GitHub']
     },
@@ -22,7 +24,8 @@ export const translationProjects = [
         name: 'Melon',
         description: 'UNES, o aplicativo que cria notificações para as ações do Portal Sagres.',
         creation: '04/10/2020',
-        link: 'https://github.com/ForceTower/Melon/pull/79/files',
+        original: 'https://github.com/ForceTower/Melon',
+        translation: 'https://github.com/ForceTower/Melon/pull/79/files',
         pr: 'https://github.com/ForceTower/Melon/pull/79',
         tags: ['strings', 'application', 'Android']
     },
@@ -31,7 +34,8 @@ export const translationProjects = [
         name: 'awesome-translations',
         description: '😎 Awesome lists about Internationalization & localization stuff. l10n, g11n, m17n, i18n. Translations! 🌎🌍.',
         creation: '27/11/2020',
-        link: 'https://github.com/JoseDeFreitas/awesome-translations/tree/es_ES#readme',
+        original: 'https://github.com/mbiesiad/awesome-translations#readme',
+        translation: 'https://github.com/JoseDeFreitas/awesome-translations/tree/es_ES#readme',
         pr: 'https://github.com/mbiesiad/awesome-translations/pull/10',
         tags: ['translations', 'list']
     },
@@ -40,7 +44,8 @@ export const translationProjects = [
         name: 'ultimate-python',
         description: 'Ultimate Python study guide for newcomers and professionals alike. 🐍 🐍 🐍.',
         creation: '27/12/2020',
-        link: 'https://github.com/huangsam/ultimate-python/blob/master/README.es.md',
+        original: 'https://github.com/huangsam/ultimate-python#readme',
+        translation: 'https://github.com/huangsam/ultimate-python/blob/master/README.es.md',
         pr: 'https://github.com/huangsam/ultimate-python/pull/37',
         tags: ['Python', 'guide']
     },
@@ -49,7 +54,8 @@ export const translationProjects = [
         name: 'learn-python',
         description: '📚 Playground and cheatsheet for learning Python. Collection of Python scripts that are split by topics and contain code examples with explanations.',
         creation: '18/01/2021',
-        link: 'https://github.com/JoseDeFreitas/learn-python/tree/spanish-translation#readme',
+        original: 'https://github.com/trekhleb/learn-python#readme',
+        translation: 'https://github.com/JoseDeFreitas/learn-python/tree/spanish-translation#readme',
         pr: 'https://github.com/trekhleb/learn-python/pull/31',
         tags: ['Python', 'guide']
     },
@@ -58,7 +64,8 @@ export const translationProjects = [
         name: 'wtfpython',
         description: 'What the f*ck Python? 😱 (in Spanish).',
         creation: '24/02/2021',
-        link: 'https://github.com/JoseDeFreitas/wtfpython-es#readme',
+        original: 'https://github.com/satwikkansal/wtfpython#readme',
+        translation: 'https://github.com/JoseDeFreitas/wtfpython-es#readme',
         pr: 'https://github.com/satwikkansal/wtfpython/pull/260',
         tags: ['Python', 'guide']
     },
@@ -66,8 +73,9 @@ export const translationProjects = [
         id: 8,
         name: 'learncpp.es',
         description: 'Contenido de learncpp.com traducido al español.',
-        creation: 'DD/MM/YYYY',
-        link: 'https://github.com/JoseDeFreitas/learncpp.es#readme',
+        creation: '09/03/2021',
+        original: 'https://learncpp.com',
+        translation: 'https://github.com/JoseDeFreitas/learncpp.es#readme',
         pr: null,
         tags: ['C++', 'guide']
     }
