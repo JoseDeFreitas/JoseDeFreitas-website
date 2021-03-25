cecd256ee62aff094ec3392a19f7c68de12e05a8
Jose De Freitas <joselocotote26@gmail.com>
2021-03-25 14:05:26 -0500

    Add project

--- public/data/data_programming.js
+++ public/data/data_programming.js
@@ -1,6 +1,14 @@
 export const programmingProjects = [
     {
         id: 1,
+        name: 'FCC Responsive Web Design',
+        description: 'Repository containing all my 5 projects from the freeCodeCamp Responsive Web Design certification.',
+        creation: '15/07/2020',
+        link: 'https://github.com/JoseDeFreitas/fcc-responsive-web-design',
+        tags: ['HTML', 'CSS', 'frontend']
+    },
+    {
+        id: 2,
         name: 'JoseDeFreitas website',
         description: 'Portfolio website of José De Freitas (this one you are seeing).',
         creation: '09/02/2021',
@@ -8,7 +16,7 @@ export const programmingProjects = [
         tags: ['portfolio', 'website', 'frontend']
     },
     {
-        id: 2,
+        id: 3,
         name: 'awesome-youtubers website voting system',
         description: '▶️ An awesome list containing awesome YouTubers that teach about technology (website).',
         creation: '07/03/2021',
@@ -16,7 +24,7 @@ export const programmingProjects = [
         tags: ['Flask', 'Python', 'database', 'api', 'backend']
     },
     {
-        id: 3,
+        id: 4,
         name: 'lintmd',
         description: 'Package to lint your markdown files easily and broadly.',
         creation: '21/03/2021',
