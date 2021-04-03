57dee68af5dc51e11699ff616da790f3e5c6a616
Jose De Freitas <joselocotote26@gmail.com>
2021-04-03 12:23:48 -0500

    Change background color

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
         <nav className="fixed h-screen">
-            <ul className="h-screen grid grid-cols-1 gap-8 place-content-center p-3 bg-first-200">
+            <ul className="h-screen grid grid-cols-1 gap-8 place-content-center p-3 bg-first-300">
                 <li>
                     <a href="/" className="p-3 rounded-lg bg-first-500 text-fourth text-lg font-extrabold hover:bg-first-600">Home</a>
                 </li>
