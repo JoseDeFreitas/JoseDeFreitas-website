473b411dfd5dacbc2c863d5d8a6ef45d39b805bf
Jose De Freitas <joselocotote26@gmail.com>
2021-04-03 13:28:49 -0500

    Change navbar padding

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,11 @@
 export default function Navbar() {
     return (
         <nav className="fixed h-screen w-48 bg-first-400 border-r-8 border-first-600">
-            <ul className="h-screen w-48 grid grid-cols-1 gap-8 place-content-center p-3">
-                <li>
-                    <a href="/" className="p-3 rounded-lg bg-first-600 text-fourth text-lg font-extrabold hover:bg-first-700">Home</a>
-                </li>
-                <li>
-                    <a href="/programming" className="p-3 rounded-lg bg-first-600 text-fourth text-lg font-extrabold hover:bg-first-700">Programming</a>
-                </li>
-                <li>
-                    <a href="/translation" className="p-3 rounded-lg bg-first-600 text-fourth text-lg font-extrabold hover:bg-first-700">Translation</a>
-                </li>
-            </ul>
+            <div className="h-screen w-48 grid grid-cols-1 gap-4 place-content-center p-3 pr-5">
+                <a href="/" className="p-3 rounded-lg bg-first-600 text-fourth text-lg font-extrabold hover:bg-first-700">Home</a>
+                <a href="/programming" className="p-3 rounded-lg bg-first-600 text-fourth text-lg font-extrabold hover:bg-first-700">Programming</a>
+                <a href="/translation" className="p-3 rounded-lg bg-first-600 text-fourth text-lg font-extrabold hover:bg-first-700">Translation</a>
+            </div>
         </nav>
     )
 }
