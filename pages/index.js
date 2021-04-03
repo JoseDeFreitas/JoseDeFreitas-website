6d883bdbaa9ec8784f164b096f25f59edb792679
Jose De Freitas <joselocotote26@gmail.com>
2021-04-03 13:25:50 -0500

    Create and style contact section

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ export default function Home() {
             </Head>
 
             <Layout>
-                <main className="pl-48 h-screen grid grid-cols-2 grid-rows-2">
-                    <section className="col-start-1 col-end-2 row-start-1 row-end-3 p-12 bg-second-400 border-r-8 border-second-600">
+                <main className="pl-48 h-screen grid grid-cols-2">
+                    <section className="col-start-1 col-end-2 p-12 bg-second-500 border-r-8 border-second-700">
                         <div className="mb-12">
                             <img src="https://avatars.githubusercontent.com/u/37962411?v=4" className="w-56 rounded-full mb-4"/>
                             <h1 className="text-fourth font-bold text-5xl">José De Freitas</h1>
@@ -25,22 +25,17 @@ export default function Home() {
                             <p className="text-fourth font-semibold text-xl">About me (bla bla bla not!)</p>
                         </div>
                     </section>
-                    <section className="col-start-2 col-end-3 row-start-1 row-end-2">
-                        <h1>Hi</h1>
-                    </section>
-                    <section className="col-start-2 col-end-3 row-start-2 row-end-3 bg-first-300">
-                        <p>Contact:</p>
-                        <ul>
-                            <li>
-                                <a href="https://github.com/JoseDeFreitas" target="_blank">GitHub</a>
-                            </li>
-                            <li>
-                                <a href="https://crowdin.com/profile/JoseDeFreitas" target="_blank">Crowdin</a>
-                            </li>
-                            <li>
-                                <a href="https://twitter.com/JoseDeF32503298" target="_blank">Twitter</a>
-                            </li>
-                        </ul>
+                    <section className="relative col-start-2 col-end-3">
+                        <div>
+                            <h1>Hi</h1>
+                        </div>
+                        <div className="absolute bg-first-400 bottom-0 w-full p-4">
+                            <div className="grid grid-cols-3 gap-4 justify-items-stretch">
+                                <a href="https://github.com/JoseDeFreitas" className="p-2 rounded-lg bg-second-500 text-fourth text-lg text-center font-bold hover:bg-second-600" target="_blank">GitHub</a>
+                                <a href="https://crowdin.com/profile/JoseDeFreitas" className="p-2 rounded-lg bg-second-500 text-fourth text-lg text-center font-bold hover:bg-second-600" target="_blank">Crowdin</a>
+                                <a href="https://twitter.com/JoseDeF32503298" className="p-2 rounded-lg bg-second-500 text-fourth text-lg text-center font-bold hover:bg-second-600" target="_blank">Twitter</a>
+                            </div>
+                        </div>
                     </section>
                 </main>
             </Layout>
