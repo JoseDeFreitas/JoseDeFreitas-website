3753410aa88d745c79fc6d361960832d6c8dd4cb
Jose De Freitas <joselocotote26@gmail.com>
2021-04-03 12:46:09 -0500

    Set homepage grid

--- pages/index.js
+++ pages/index.js
@@ -14,45 +14,21 @@ export default function Home() {
             </Head>
 
             <Layout>
-                <main className="h-screen">
-                    <section>
-                        <h1>José De Freitas</h1>
-                        <p>Programmer and translator</p>
-                        <a href="https://ko-fi.com/D1D62AAG8" target="_blank"><img src="https://ko-fi.com/img/githubbutton_sm.svg" alt="Ko-fi button"/></a>
-                    </section>
-                    <section>
+                <main className="pl-48 h-screen grid grid-cols-2 grid-rows-2">
+                    <section className="col-start-1 col-end-2 row-start-1 row-end-3 p-12">
                         <div>
-                            <h2>Programming</h2>
-                            <p>
-                                I code various types of programs. I like many topics, but to
-                                list some of them, I'd say: automation, web development
-                                (front-end and back-end), web applications, desktop applications,
-                                networking, and more. I also do coding challenges very often, as I
-                                find them pretty fun and they make me gain more skills.
-                            </p>
-                            <p>
-                                I use Python, C++ and C# as programming languages, though
-                                I also know JavaScript, HTML and CSS (sass). About more specific
-                                technologies I know git and the main IDE I use is Visual Studio Code.
-                            </p>
+                            <img src="https://avatars.githubusercontent.com/u/37962411?v=4" className="w-56 rounded-full"/>
+                            <h1>José De Freitas</h1>
+                            <p>Programmer and translator</p>
                         </div>
                         <div>
-                            <h2>Translation</h2>
-                            <p>
-                                I'm a native Spanish speaker that have been lerning English years ago.
-                                I started to translate documents some time ago and it's something I
-                                really enjoy. I have a very meticulous process when it comes to
-                                doing a translation job, as I consider a translation should reflect
-                                the same ideas and content of the original document.
-                            </p>
-                            <p>
-                                I've translated numerous documents. The topics of these documents
-                                are mainly tech-related and of various different topics within this
-                                broad field.
-                            </p>
+                            <p>About me (bla bla bla not!)</p>
                         </div>
                     </section>
-                    <section>
+                    <section className="col-start-2 col-end-3 row-start-1 row-end-2">
+                        <h1>Hi</h1>
+                    </section>
+                    <section className="col-start-2 col-end-3 row-start-2 row-end-3 bg-first-300">
                         <p>Contact:</p>
                         <ul>
                             <li>
