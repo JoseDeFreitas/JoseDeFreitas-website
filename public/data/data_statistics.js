57cb7761071b0fb40e232030aab3611f89492993
Jose De Freitas <joselocotote26@gmail.com>
2021-04-04 11:51:55 -0500

    Create statistical data

---
+++ public/data/data_statistics.js
@@ -0,0 +1,52 @@
+export const statsOverall = [
+    {
+        name: 'Programming',
+        date: '01/2018'
+    },
+    {
+        name: 'Translation',
+        date: '01/2020'
+    }
+]
+
+export const statsPLanguages = [
+    {
+        name: 'Python',
+        date: '01/2020'
+    },
+    {
+        name: 'JavaScript',
+        date: '01/2018'
+    },
+    {
+        name: 'HTML',
+        date: '01/2018'
+    },
+    {
+        name: 'CSS',
+        date: '01/2018'
+    },
+    {
+        name: 'C#',
+        date: '02/2021'
+    },
+    {
+        name: 'Markdown',
+        date: '03/2019'
+    },
+    {
+        name: 'XML',
+        date: '02/2021'
+    }
+]
+
+export const statsTechnologies = [
+    {
+        name: 'Git',
+        date: '04/2020'
+    },
+    {
+        name: 'Visual Studio Code',
+        date: '03/2019'
+    }
+]
