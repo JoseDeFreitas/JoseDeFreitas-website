9739e4ce10203033896192067ef0a33c06b49119
Jose De Freitas <joselocotote26@gmail.com>
2021-04-04 12:15:12 -0500

    Add badge color

--- public/data/data_statistics.js
+++ public/data/data_statistics.js
@@ -1,52 +1,63 @@
 export const statsOverall = [
     {
         name: 'Programming',
-        date: '01/2018'
+        date: '01/2018',
+        color: 'gray'
     },
     {
         name: 'Translation',
-        date: '01/2020'
+        date: '01/2020',
+        color: 'gray'
     }
 ]
 
 export const statsPLanguages = [
     {
         name: 'Python',
-        date: '01/2020'
+        date: '01/2020',
+        color: 'blue'
     },
     {
         name: 'JavaScript',
-        date: '01/2018'
+        date: '01/2018',
+        color: 'yellow'
     },
     {
         name: 'HTML',
-        date: '01/2018'
+        date: '01/2018',
+        color: 'orange'
     },
     {
         name: 'CSS',
-        date: '01/2018'
+        date: '01/2018',
+        color: 'purple'
     },
     {
         name: 'C#',
-        date: '02/2021'
+        date: '02/2021',
+        color: 'green'
     },
     {
         name: 'Markdown',
-        date: '03/2019'
+        date: '03/2019',
+        color: 'gray'
     },
     {
         name: 'XML',
-        date: '02/2021'
+        date: '02/2021',
+        color: 'gray'
     }
 ]
 
 export const statsTechnologies = [
     {
         name: 'Git',
-        date: '04/2020'
+        date: '04/2020',
+        color: 'gray'
     },
     {
         name: 'Visual Studio Code',
-        date: '03/2019'
+        date: '03/2019',
+        color: 'gray'
     }
 ]
