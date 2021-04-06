aedddae9798a5a134c8db55c6b49d247510bdb67
Jose De Freitas <joselocotote26@gmail.com>
2021-04-06 17:28:51 -0500

    Add mode toggle functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import 'tailwindcss/tailwind.css'
+import { ThemeProvider } from 'next-themes'
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <ThemeProvider attribute="class">
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
 }
