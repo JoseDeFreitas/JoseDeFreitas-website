2786b39eade822a5af22d2384acc7ffb9916e23b
Jose De Freitas <joselocotote26@gmail.com>
2021-04-06 19:47:37 -0500

    Add top navbar menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,9 @@ export default function Navbar() {
     const { theme, setTheme } = useTheme()
     return (
         <nav className="fixed h-screen w-48 bg-first-200 border-r-2 border-first-300 dark:bg-first-800 dark:border-first-900">
-            <button aria-label="Toggle Dark Mode" type="button" className="absolute m-3 py-1 px-2 rounded text-first-700 text-sm text-center font-semibold focus:outline-none focus:ring focus:border-first-500 border-2 border-first-400 hover:bg-first-300 dark:text-first-50 dark:border-first-900 dark:hover:bg-first-900" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>{theme === 'dark' ? "light" : "dark"}</button>
+            <div className="absolute w-full p-3 left-0 grid grid-flow-col gap-2 justify-items-stretch">
+                <button aria-label="Toggle Dark Mode" type="button" className="py-1 px-2 rounded text-first-700 text-sm text-center font-semibold focus:outline-none focus:ring focus:border-first-500 border-2 border-first-400 hover:bg-first-300 dark:text-first-50 dark:border-first-900 dark:hover:bg-first-900" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>{theme === 'dark' ? "light" : "dark"}</button>
+            </div>
             <div className="h-screen w-48 grid grid-cols-1 gap-2 place-content-center p-3 pr-4">
                 <Link href="/">
                     <a className="p-2 rounded text-base font-semibold text-first-800 hover:bg-first-300 dark:text-first-100 dark:hover:bg-first-900">Home</a>
