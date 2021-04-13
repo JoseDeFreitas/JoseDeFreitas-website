3faf34c4a44a32413712c5098b195ce49a6508df
Jose De Freitas <joselocotote26@gmail.com>
2021-04-13 09:09:05 -0500

    Change button spacing

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
             <div className="absolute w-auto pt-1 pr-5 right-0 top-0 grid grid-flow-col gap-2 justify-items-stretch xl:w-full xl:right-auto xl:top-auto xl:left-0 xl:p-3">
                 <button aria-label="Toggle Dark Mode" type="button" className="px-1 rounded text-first-700 text-sm text-center font-semibold focus:outline-none focus:ring focus:border-first-500 border-2 border-first-400 hover:bg-first-300 dark:text-first-50 dark:border-first-900 dark:hover:bg-first-900 xl:py-1 xl:px-2" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>{theme === 'dark' ? "light" : "dark"}</button>
             </div>
-            <div className="h-16 w-screen grid grid-flow-col gap-3 place-content-center p-2 pr-12 xl:h-screen xl:w-48 xl:grid-flow-row xl:grid-cols-1 xl:gap-2 xl:p-3 xl:pr-4">
+            <div className="h-16 w-screen grid grid-flow-col gap-1 place-content-center p-2 pr-12 xl:h-screen xl:w-48 xl:grid-flow-row xl:grid-cols-1 xl:gap-2 xl:p-3 xl:pr-4">
                 <Link href="/">
                     <a className="p-2 rounded text-base font-semibold text-first-800 hover:bg-first-300 dark:text-first-100 dark:hover:bg-first-900">Home</a>
                 </Link>
