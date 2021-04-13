2237224c33d81350e6d5267e2c49aeaf46ce8d9b
Jose De Freitas <joselocotote26@gmail.com>
2021-04-13 13:40:30 -0500

    Change dark theme colors

--- pages/programming.js
+++ pages/programming.js
@@ -19,7 +19,7 @@ export default function Programming({ projectsList }) {
 
             <Layout>
                 <main className="pt-16 h-screen grid grid-flow-row xl:grid-cols-4 xl:pl-48 xl:pt-0">
-                    <section className="p-6 bg-first-50 border-b-2 border-first-300 dark:bg-first-700 dark:border-first-900 xl:border-b-0 xl:border-r-2 xl:col-start-1 xl:col-end-2">
+                    <section className="p-6 bg-first-50 border-b-2 border-first-300 dark:bg-first-900 dark:border-first-700 xl:border-b-0 xl:border-r-2 xl:col-start-1 xl:col-end-2">
                         <h1 className="font-bold text-first-800 text-2xl mb-2 dark:text-first-100">Programming</h1>
                         <p className="font-semibold text-justify text-first-600 dark:text-first-300">
                             At the left you can see all my programming projects. All of them are open
@@ -38,10 +38,10 @@ export default function Programming({ projectsList }) {
                             with other libraries in order to provide a good product.
                         </p>
                     </section>
-                    <section className="p-6 bg-first-50 dark:bg-first-700 xl:col-start-2 xl:col-end-5">
+                    <section className="p-6 bg-first-50 dark:bg-first-900 xl:col-start-2 xl:col-end-5">
                         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 2xl:grid-cols-3">
                             {projectsList.map(project => (
-                                <div className="relative bg-first-200 border-2 border-first-400 p-3 rounded divide-y-2 divide-first-400 dark:bg-first-800 dark:border-first-900 dark:divide-first-900">
+                                <div className="relative bg-first-200 border-2 border-first-400 p-3 rounded divide-y-2 divide-first-400 dark:bg-first-800 dark:border-first-600 dark:divide-first-600">
                                     <div className="mb-14">
                                         <h3 className="text-first-800 font-semibold text-lg dark:text-first-100">{project.name}</h3>
                                         <p className="text-first-700 dark:text-first-200">{project.description}</p>
@@ -55,7 +55,7 @@ export default function Programming({ projectsList }) {
                                     </div>
                                     <div className="absolute bottom-0 left-0 w-full p-2">
                                         <div className="grid grid-flow-col gap-4 justify-items-stretch">
-                                            <a href={project.link} className="p-1 rounded text-first-800 text-base text-center font-semibold border-2 border-first-400 hover:bg-first-300 dark:text-first-50 dark:border-first-900 dark:hover:bg-first-900" target="_blank">Project</a>
+                                            <a href={project.link} className="p-1 rounded text-first-800 text-base text-center font-semibold border-2 border-first-400 hover:bg-first-300 dark:text-first-50 dark:border-first-600 dark:hover:bg-first-900" target="_blank">Project</a>
                                         </div>
                                     </div>
                                 </div>
--- pages/translation.js
+++ pages/translation.js
@@ -19,7 +19,7 @@ export default function Translation({ projectsList }) {
 
             <Layout>
                 <main className="pt-16 h-screen grid grid-flow-row xl:grid-cols-4 xl:pl-48 xl:pt-0">
-                    <section className="p-6 bg-first-50 border-b-2 border-first-300 dark:bg-first-700 dark:border-first-900 xl:border-b-0 xl:border-r-2 xl:col-start-1 xl:col-end-2">
+                    <section className="p-6 bg-first-50 border-b-2 border-first-300 dark:bg-first-900 dark:border-first-700 xl:border-b-0 xl:border-r-2 xl:col-start-1 xl:col-end-2">
                         <h1 className="font-bold text-first-800 text-2xl mb-2 dark:text-first-100">Translation</h1>
                         <p className="font-semibold text-justify text-first-600 dark:text-first-300">
                             I'm a native Spanish speaker who has been interested in languages since a
@@ -36,10 +36,10 @@ export default function Translation({ projectsList }) {
                             TXT and RST.
                         </p>
                     </section>
-                    <section className="p-6 bg-first-50 dark:bg-first-700 xl:col-start-2 xl:col-end-5">
+                    <section className="p-6 bg-first-50 dark:bg-first-900 xl:col-start-2 xl:col-end-5">
                         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 2xl:grid-cols-3">
                             {projectsList.map(project => (
-                                <div className="relative bg-first-200 border-2 border-first-400 p-3 rounded divide-y-2 divide-first-400 dark:bg-first-800 dark:border-first-900 dark:divide-first-900">
+                                <div className="relative bg-first-200 border-2 border-first-400 p-3 rounded divide-y-2 divide-first-400 dark:bg-first-800 dark:border-first-600 dark:divide-first-600">
                                     <div className="mb-14">
                                         <h3 className="text-first-800 font-semibold text-lg dark:text-first-100">{project.name}</h3>
                                         <p className="text-first-700 dark:text-first-200">{project.description}</p>
@@ -53,9 +53,9 @@ export default function Translation({ projectsList }) {
                                     </div>
                                     <div className="absolute bottom-0 left-0 w-full p-2">
                                         <div className="grid grid-flow-col gap-4 justify-items-stretch">
-                                            <a href={project.source} className="p-1 rounded text-first-800 text-base text-center font-semibold border-2 border-first-400 hover:bg-first-300 dark:text-first-50 dark:border-first-900 dark:hover:bg-first-900" target="_blank">Source</a>
-                                            <a href={project.translation} className="p-1 rounded text-first-800 text-base text-center font-semibold border-2 border-first-400 hover:bg-first-300 dark:text-first-50 dark:border-first-900 dark:hover:bg-first-900" target="_blank">Translation</a>
-                                            {project.pr ? <a href={project.pr} className="p-1 rounded text-first-800 text-base text-center font-semibold border-2 border-first-400 hover:bg-first-300 dark:text-first-50 dark:border-first-900 dark:hover:bg-first-900" target="_blank">Context</a> : null}
+                                            <a href={project.source} className="p-1 rounded text-first-800 text-base text-center font-semibold border-2 border-first-400 hover:bg-first-300 dark:text-first-50 dark:border-first-600 dark:hover:bg-first-900" target="_blank">Source</a>
+                                            <a href={project.translation} className="p-1 rounded text-first-800 text-base text-center font-semibold border-2 border-first-400 hover:bg-first-300 dark:text-first-50 dark:border-first-600 dark:hover:bg-first-900" target="_blank">Translation</a>
+                                            {project.pr ? <a href={project.pr} className="p-1 rounded text-first-800 text-base text-center font-semibold border-2 border-first-400 hover:bg-first-300 dark:text-first-50 dark:border-first-600 dark:hover:bg-first-900" target="_blank">Context</a> : null}
                                         </div>
                                     </div>
                                 </div>
