37b9ad507e73f80b17f2add511c04c6788a464a0
Jose De Freitas <joselocotote26@gmail.com>
2021-04-13 11:45:36 -0500

    Make styles responsive

--- pages/translation.js
+++ pages/translation.js
@@ -18,10 +18,10 @@ export default function Translation({ projectsList }) {
             </Head>
 
             <Layout>
-                <main className="pl-48 h-screen grid grid-cols-4">
-                    <section className="col-start-1 col-end-2 p-10 bg-first-50 border-r-2 border-first-300 dark:bg-first-700 dark:border-first-900">
+                <main className="pt-16 h-screen grid grid-flow-row xl:grid-cols-4 xl:pl-48 xl:pt-0">
+                    <section className="p-6 bg-first-50 border-b-2 border-first-300 dark:bg-first-700 dark:border-first-900 xl:border-b-0 xl:border-r-2 xl:col-start-1 xl:col-end-2">
                         <h1 className="font-bold text-first-800 text-2xl mb-2 dark:text-first-100">Translation</h1>
-                        <p className="font-semibold text-first-600 dark:text-first-300">
+                        <p className="font-semibold text-justify text-first-600 dark:text-first-300">
                             I'm a native Spanish speaker who has been interested in languages since a
                             long time ago.<br/><br/>
                             The "Source" button links to the original document from the language I
@@ -36,8 +36,8 @@ export default function Translation({ projectsList }) {
                             TXT and RST.
                         </p>
                     </section>
-                    <section className="col-start-2 col-end-5 p-6 bg-first-50 overflow-y-auto dark:bg-first-700">
-                        <div className="grid grid-cols-3 gap-4 mb-6">
+                    <section className="p-6 bg-first-50 dark:bg-first-700 xl:col-start-2 xl:col-end-5">
+                        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 2xl:grid-cols-3">
                             {projectsList.map(project => (
                                 <div className="relative bg-first-200 border-2 border-first-400 p-3 rounded divide-y-2 divide-first-400 dark:bg-first-800 dark:border-first-900 dark:divide-first-900">
                                     <div className="mb-14">
