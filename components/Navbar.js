0f7fa115331cc88855d7b1faf6fdb94c29325b69
Jose De Freitas <joselocotote26@gmail.com>
2021-04-13 13:33:50 -0500

    Change dark theme colors

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,9 +4,9 @@ import { useTheme } from 'next-themes'
 export default function Navbar() {
     const { theme, setTheme } = useTheme()
     return (
-        <nav className="fixed z-20 h-16 w-screen bg-first-200 border-b-2 border-first-300 dark:bg-first-800 dark:border-first-900 xl:h-screen xl:w-48 xl:border-b-0 xl:border-r-2">
+        <nav className="fixed z-20 h-16 w-screen bg-first-200 border-b-2 border-first-300 dark:bg-first-800 dark:border-first-700 xl:h-screen xl:w-48 xl:border-b-0 xl:border-r-2">
             <div className="absolute w-auto pt-1 pr-5 right-0 top-0 grid grid-flow-col gap-2 justify-items-stretch xl:w-full xl:right-auto xl:top-auto xl:left-0 xl:p-3">
-                <button aria-label="Toggle Dark Mode" type="button" className="px-1 rounded text-first-700 text-sm text-center font-semibold focus:outline-none focus:ring focus:border-first-500 border-2 border-first-400 hover:bg-first-300 dark:text-first-50 dark:border-first-900 dark:hover:bg-first-900 xl:py-1 xl:px-2" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>{theme === 'dark' ? "light" : "dark"}</button>
+                <button aria-label="Toggle Dark Mode" type="button" className="px-1 rounded text-first-700 text-sm text-center font-semibold focus:outline-none focus:ring focus:border-first-500 border-2 border-first-400 hover:bg-first-300 dark:text-first-50 dark:border-first-600 dark:hover:bg-first-900 xl:py-1 xl:px-2" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>{theme === 'dark' ? "light" : "dark"}</button>
             </div>
             <div className="h-16 w-screen grid grid-flow-col gap-1 place-content-center p-2 pr-12 xl:h-screen xl:w-48 xl:grid-flow-row xl:grid-cols-1 xl:gap-2 xl:p-3 xl:pr-4">
                 <Link href="/">
