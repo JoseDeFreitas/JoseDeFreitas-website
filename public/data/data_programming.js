c009463bd27a4e82dfb82b3519910a8056d456d0
José De Freitas <37962411+JoseDeFreitas@users.noreply.github.com>
2021-04-22 17:26:49 -0500

    Remove a project

--- public/data/data_programming.js
+++ public/data/data_programming.js
@@ -13,11 +13,4 @@ export const programmingProjects = [
         link: 'https://github.com/JoseDeFreitas/JoseDeFreitas-website',
         tags: ['Next.js', 'Tailwind', 'frontend']
     },
-    {
-        name: 'awesome-youtubers website voting system',
-        description: '▶️ An awesome list containing awesome YouTubers that teach about technology (website).',
-        creation: '07/03/2021',
-        link: 'https://github.com/JoseDeFreitas/awesome-youtubers/tree/main/src/voter',
-        tags: ['Flask', 'Python', 'database', 'api', 'backend']
-    },
 ]
